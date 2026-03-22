f8219bba4f3a8f75bfb0867e6da0da88ce766d30
ruwani <ruwanielv@gmail.com>
2026-03-22 22:55:36 +0530

    fix error in schema.prisma

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,15 @@
+// import "dotenv/config";
+// import { PrismaClient } from "../../generated/prisma/client";
+
+// const prisma = new PrismaClient();
+// export { PrismaClient };
+// export { prisma };
+
 import "dotenv/config";
 import { PrismaClient } from "../../generated/prisma/client";
 
-const prisma = new PrismaClient();
-export { PrismaClient };
-export { prisma };
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
+
+export { PrismaClient, prisma };
