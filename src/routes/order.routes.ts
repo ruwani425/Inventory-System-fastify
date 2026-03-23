1436db9a33d87b6e7a8a517cb56964d9cc714a74
ruwani <ruwanielv@gmail.com>
2026-03-23 08:18:47 +0530

    complete jwt part

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { placeorder } from "../controllers/order.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 export const orderRoutes = async (app: FastifyInstance) => {
-  app.post("/orders", async (req, reply) => {
+  app.post("/orders", { preHandler: verifyToken }, async (req, reply) => {
     const { customerId, productId, quantity } = req.body as {   customerId: string; productId: string; quantity: number };
     try {
       return await placeorder(customerId, productId, quantity); 
